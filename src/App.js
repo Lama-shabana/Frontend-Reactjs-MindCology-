de367c075af3544eb75573382e14662b8a23f8f4
Lama <lamashabana228@gmail.com>
2021-03-06 01:09:51 +0200

    test3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import {ToastProvider} from 'react-toast-notifications'
 import Content from "./Components/Content/Content";
+
 // import
 function App() {
   return (
       <React.Fragment>
 
           //test2
-
+//test3
         {/*<InterfaceUtility/>*/}
         <ToastProvider>
           {/*<Layout {...props}>*/}
