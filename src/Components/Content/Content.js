e23140c933c7ea5509cad5aab65faf7150d11139
Lama <lamashabana228@gmail.com>
2021-03-14 22:01:24 +0200

    add-content

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import EnglishHomepage from "../../Containers/Homepage/EnglishHomepage";
 import Registration from "../../Containers/Registration/Registration";
 import ArabicHomepage from "../../Containers/Homepage/ArabicHomepage";
+import About from "../../Containers/Homepage/About/About";
+import Contact from "../../Containers/Homepage/Contact/Contact";
 const Content = (props) => {
     return (
         <div>
@@ -9,7 +11,8 @@ const Content = (props) => {
                 <Switch>
                     <Route path="/" exact component={EnglishHomepage}/>
                     <Route path="/arabicHomepage" exact component={ArabicHomepage}/>
-
+                    <Route path="/about" exact component={About}/>
+                    <Route path="/contact" exact component={Contact}/>
                     <Route path="/registration" exact component={Registration}/>
 
                     {/*<Route path="/homepage" exact component={}/>*/}
