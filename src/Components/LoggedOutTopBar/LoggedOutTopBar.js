9724750c9f16e809263a8857d5b8663a678f4515
Hadeel Nezar <nez.hadeel@gmail.com>
2021-04-11 11:27:19 +0300

    fix header when logged in

--- src/Components/LoggedOutTopBar/LoggedOutTopBar.js
+++ src/Components/LoggedOutTopBar/LoggedOutTopBar.js
@@ -11,60 +11,113 @@ const LoggedOutTopBar = (props) => {
     let user = "";
     return (
         // <Sticky className={classes.stickyBackground}>
-        <div className="p-grid">
-            {/*{console.log(props.history.location?.pathname,"path")}*/}
+        <div>
+            {user.length?
+                <div className="p-grid">
+                    {console.log(user.length,"here")}
 
-            <div className="p-col-2">
-                <a href="/">
-                    <img src={MindCology} className={classes.image}
-                         alt="logo"/>
-                </a>
-            </div>
-            <div className="p-col-1">
-                <a href="/" className={classes.links}>Home</a>
-            </div>
-            <div className="p-col-1">
-                <a href="/about" className={classes.links}>About</a>
+                    {/*{console.log(props.history.location?.pathname,"path")}*/}
 
-            </div>
-            <div className="p-col-1">
-                <a href="/contact" className={classes.links}>Contact Us</a>
 
-            </div>
 
-                {window.location.pathname !== "/registration" &&!user ?
-                    <div className="p-col-1">
-                        <a href="/registration" className={classes.links}>Sign Up </a>
+                    <div className="p-col-2">
+                        <a href="/">
+                            <img src={MindCology} className={classes.image}
+                                 alt="logo"/>
+                        </a>
                     </div>
-                    : null}
-
-
-                {window.location.pathname !== "/login" &&!user?
                     <div className="p-col-1">
-                        <a href="/login" className={classes.links}>Login </a>
+                        <a href="/" className={classes.links}>Home</a>
                     </div>
-                    : <div className="p-col-1"/>}
+                    <div className="p-col-1">
+                        <a href="/about" className={classes.links}>About</a>
 
-                {window.location.pathname === "/registration" &&!user?
+                    </div>
+                    <div className="p-col-1">
+                        <a href="/contact" className={classes.links}>Contact Us</a>
+
+                    </div>
+
+                    {window.location.pathname !== "/registration"?
+                        <div className="p-col-1">
+                            <a href="/registration" className={classes.links}>Sign Up </a>
+                        </div>
+                        : null}
+
+
+                    {window.location.pathname !== "/login"?
+                        <div className="p-col-1">
+                            <a href="/login" className={classes.links}>Login </a>
+                        </div>
+                        : <div className="p-col-1"/>}
+
+                    {window.location.pathname === "/registration"?
+                        <div className="p-col-3"/>
+
+                        : <div className="p-col-2"/>}
+
+
+
+
+                    <div className="p-col-2">
+                        <div className="dropdown" style={{marginTop: "2em"}}>
+                            <button className="dropbtn">English</button>
+                            <div className="dropdown-content">
+                                <a href="/">English</a>
+                                <a href="/ArabicHomepage"> Arabic </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-col-1"/>
+                </div>
+                :
+
+                <div className="p-grid">
+                    {console.log(user.length,"here")}
+
+                    {/*{console.log(props.history.location?.pathname,"path")}*/}
+
+
+
+                    <div className="p-col-2">
+                        <a href="/">
+                            <img src={MindCology} className={classes.image}
+                                 alt="logo"/>
+                        </a>
+                    </div>
+                    <div className="p-col-1">
+                        <a href="/" className={classes.links}>Home</a>
+                    </div>
+                    <div className="p-col-1">
+                        <a href="/about" className={classes.links}>About</a>
+
+                    </div>
+                    <div className="p-col-1">
+                        <a href="/contact" className={classes.links}>Contact Us</a>
+
+                    </div>
+
+
                     <div className="p-col-3"/>
 
-                    : <div className="p-col-2"/>}
-
-
-
-
-            <div className="p-col-2">
-                <div className="dropdown" style={{marginTop: "2em"}}>
-                    <button className="dropbtn">English</button>
-                    <div className="dropdown-content">
-                        <a href="/">English</a>
-                        <a href="/ArabicHomepage"> Arabic </a>
+                    <div className="p-col-2">
+                        <div className="dropdown" style={{marginTop: "2em"}}>
+                            <button className="dropbtn">English</button>
+                            <div className="dropdown-content">
+                                <a href="/">English</a>
+                                <a href="/ArabicHomepage"> Arabic </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="p-col-1"/>
+                    <div className="p-col-1"/>
+                </div>
+
+
+            }
         </div>
+
         // </Sticky>
 
 
