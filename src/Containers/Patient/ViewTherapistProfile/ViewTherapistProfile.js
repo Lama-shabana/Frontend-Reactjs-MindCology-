f17de2aa43ebe5bdeda997ca3ee4aa362b1c0557
Hadeel Nezar <nez.hadeel@gmail.com>
2021-05-03 01:31:52 +0300

    fix label and add grid

--- src/Containers/Patient/ViewTherapistProfile/ViewTherapistProfile.js
+++ src/Containers/Patient/ViewTherapistProfile/ViewTherapistProfile.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-import classes from '../../Therapist/TherapistDashboard/therapistProfile.module.css';
+import classes from './ViewTherapistProfile.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Tab, Tabs} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,9 +8,9 @@ import "mdbreact/dist/css/mdb.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import profile from "../assets/profile.png";
-import {MDBBtn,MDBCardBody,MDBCardImage,MDBCardTitle,MDBCardText,} from "mdbreact";
-import {MDBCard,MDBRow,MDBCol,MDBIcon} from "mdbreact";
+import profile from "../assets/profileTesting.jpg";
+import {MDBBtn, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText,} from "mdbreact";
+import {MDBCard, MDBRow, MDBCol, MDBIcon} from "mdbreact";
 import {Button} from "primereact/button";
 import * as profileActions from "../../Therapist/store/TherapistActions";
 import {Icon} from "@material-ui/core";
@@ -20,24 +20,20 @@ const ViewTherapistProfile = (props) => {
     const [therapistInfo, setTherapistInfo] = useState(null)
 
 
-
     const userId = JSON.parse(localStorage.getItem("auth"))?.id
 
-    const therapistId=props.match.params.id;
+    const therapistId = props.match.params.id;
 
     let dataLoaded = false;
     useEffect(() => {
 
         if (dataLoaded === false) {
-            props.getAllTherapists({id:therapistId}).then((data) => {
+            props.getAllTherapists({id: therapistId}).then((data) => {
                 setTherapistInfo(data.payload[0])
             })
             dataLoaded = true;
         }
     }, [dataLoaded])
-
-
-
 
 
     const therapistInfoDisplay = () => {
@@ -85,8 +81,6 @@ const ViewTherapistProfile = (props) => {
                     </label>
 
 
-
-
                 </div>
 
 
@@ -101,14 +95,14 @@ const ViewTherapistProfile = (props) => {
 
                         {/*<MDBIcon fab icon="phone" className="mr-1" />*/}
                         {/*<i icon="pi pi-clock"  style={{'fontSize': '2em'}} ></i>*/}
-                        <label icon="pi pi-clock" className={classes.labels}>  Telephone </label>
+                        <label icon="pi pi-clock" className={classes.labels}> Telephone </label>
                     </MDBCardText>
                 </div>
                 <div className="p-col-4">
                     <MDBCardText>
 
-                        <label  className={classes.labels} >
-                            <MDBIcon fab className="email-icons" className="mr-1" />
+                        <label className={classes.labels}>
+                            <MDBIcon fab className="email-icons" className="mr-1"/>
                             Email </label>
                     </MDBCardText>
                 </div>
@@ -127,8 +121,6 @@ const ViewTherapistProfile = (props) => {
                     <label>
                         {therapistInfo.phoneNumber}
                     </label>
-
-
 
 
                 </div>
@@ -151,7 +143,6 @@ const ViewTherapistProfile = (props) => {
                 {/*    /!*<label className={classes.labels}>user-name</label>*!/*/}
 
                 {/*</div>*/}
-
 
 
                 <div className="p-col-4" style={{textAlign: "center"}}>
@@ -205,7 +196,6 @@ const ViewTherapistProfile = (props) => {
                 <div>
 
 
-
                 </div>
 
             </div>
@@ -217,51 +207,123 @@ const ViewTherapistProfile = (props) => {
 
     return (
         <div className={classes.form}>
-            {therapistInfo ?
-
-                <div>
-                    <MDBRow>
-                        <MDBCol md="5">
-                            <MDBCard cascade>
-                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <img style={{width:"150px",height:"8em",paddingLeft:"1em" }}
-                                        src={profile}
-                                        className="img-fluid"
-                                    />
-                                    <div className="" style={{paddingTop:'1em',paddingLeft:"1em"}}>
-                                    <span>
-                       <Button  icon="pi pi-comments" style={{marginRight: '.25em'}}/>
-                      <Button icon="pi pi-clock" className="p-button-secondary"/>
-</span></div>
-                                </div>
-                                <label className={classes.headerLabel}>
-                                    {/*<MDBCardImage*/}
-                                    {/*                        src={profile}*/}
-                                    {/*                    />*/}
-
-                                </label>
-
-                                <Tabs defaultActiveKey="therapistInfo" transition={false}>
-                                    <Tab eventKey="therapistInfo"
-                                         title={therapistInfo.username} tabClassName={classes.tab}>
-                                        {therapistInfoDisplay()}
-                                    </Tab>
-
-                                </Tabs>
-                                {/*<div className="p-grid">*/}
-
-                                {/*    <div className="p-col-5"/>*/}
-
-                                {/*    <div className="p-col-4"/>*/}
-                                {/*</div>*/}
-
-
-                            </MDBCard>
-                        </MDBCol>
-                    </MDBRow>
+            <div className="p-grid">
+                <div className="p-col-4">
+                    <img
+                        width={300}
+                        // style={{width: "22em", height: "16em", paddingLeft: "1em"}}
+                        src={profile}/>
                 </div>
 
-                : null}
+                <div className="p-col-8">
+                    <div className="p-grid">
+                        <div className="p-col-12">
+                            <h2>
+                                Full Name
+                            </h2>
+                        </div>
+                        <div className="p-col-12" >
+                            <hr style={{margin:"0"}}/>
+                        </div>
+                        <div className="p-col-12" >
+                            <p>
+                                We will write our paragraph here.
+                                <i style={{color:"red"}} className="pi pi-clock"/>
+
+                            </p>
+                        </div>
+                    </div>
+              </div>
+                <div className="p-col-12">
+                    <p>5 years of experience</p>
+                </div>
+                <div className="p-col-12">
+                    <p>5 years of experience</p>
+                </div>
+
+
+            </div>
+            {/*{therapistInfo ?*/
+            }
+
+            {/*    <div>*/
+            }
+            {/*        <MDBRow>*/
+            }
+            {/*            <MDBCol md="5">*/
+            }
+            {/*                <MDBCard cascade>*/
+            }
+            {/*                    <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">*/
+            }
+            {/*                        <img style={{width: "150px", height: "8em", paddingLeft: "1em"}}*/
+            }
+            {/*                             src={profile}*/
+            }
+            {/*                             className="img-fluid"*/
+            }
+            {/*                        />*/
+            }
+            {/*                        <div className="" style={{paddingTop: '1em', paddingLeft: "1em"}}>*/
+            }
+            {/*                        <span>*/
+            }
+            {/*           <Button icon="pi pi-comments" style={{marginRight: '.25em'}}/>*/
+            }
+            {/*          <Button icon="pi pi-clock" className="p-button-secondary"/>*/
+            }
+            {/*    </span></div>*/
+            }
+            {/*                    </div>*/
+            }
+            {/*                    <label className={classes.headerLabel}>*/
+            }
+            {/*                        /!*<MDBCardImage*!/*/
+            }
+            {/*                        /!*                        src={profile}*!/*/
+            }
+            {/*                        /!*                    />*!/*/
+            }
+
+            {/*                    </label>*/
+            }
+
+            {/*                    <Tabs defaultActiveKey="therapistInfo" transition={false}>*/
+            }
+            {/*                        <Tab eventKey="therapistInfo"*/
+            }
+            {/*                             title={therapistInfo.username} tabClassName={classes.tab}>*/
+            }
+            {/*                            {therapistInfoDisplay()}*/
+            }
+            {/*                        </Tab>*/
+            }
+
+            {/*                    </Tabs>*/
+            }
+            {/*                    /!*<div className="p-grid">*!/*/
+            }
+
+            {/*                    /!*    <div className="p-col-5"/>*!/*/
+            }
+
+            {/*                    /!*    <div className="p-col-4"/>*!/*/
+            }
+            {/*                    /!*</div>*!/*/
+            }
+
+
+            {/*                </MDBCard>*/
+            }
+            {/*            </MDBCol>*/
+            }
+            {/*        </MDBRow>*/
+            }
+            {/*    </div>*/
+            }
+
+            {/*    : null}*/
+            }
 
         </div>
     );
